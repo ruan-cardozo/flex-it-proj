9a856b00d8dea0dd25b1af69adb939731cbe2701
ruan-cardozo <ruanvcardozo@gmail.com>
2024-11-07 23:24:20 -0300

    adding auth context to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,30 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import HomePage from './pages/HomePage/HomePage'
-import TrainingPage from './pages/TrainingPage/TrainingPage'
-import LoginPage from './pages/LoginPage/LoginPage'
-import RegisterPage from './pages/RegisterPage/RegisterPage'
-import Metrics from './pages/MetricasPage/Metrics'
-import DietPage from './pages/DietPage/DietPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+import ProtectedRoute from './routes/ProtectedRoute';
+import LoginPage from './pages/LoginPage/Login';
+import RegisterPage from './pages/RegisterPage/Register';
+import HomePage from './pages/HomePage/HomePage';
+import Metrics from './pages/MetricasPage/Metrics';
+import TrainingPage from './pages/TrainingPage/TrainingPage';
+import DietPage from './pages/DietPage/DietPage';
 
-
-function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/home' element={<HomePage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route path='/' element={<RegisterPage />} />
-				<Route path='/metricas' element={<Metrics />} /> 
-				<Route path='/treinos' element={<TrainingPage />} />
-				<Route path='/dietas' element={<DietPage />} />
-			</Routes>
-		</BrowserRouter>
-	);
-}
-
+const App = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/' element={<RegisterPage />} />
+		  <Route element={<ProtectedRoute />}>
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/metricas' element={<Metrics />} />
+            <Route path='/treinos' element={<TrainingPage />} />
+            <Route path='/dietas' element={<DietPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
 
 export default App;
